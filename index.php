d4b66f0dbacb4d60f945ce7ff9e16a5cb0d9f8bd
david <david@ongoingworlds.com>
2015-07-12 12:47:33 +0100

    All of Bootstrap and all of my overrides and amendments

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include("_top.php"); ?>
+
+<div class="container-wrapper">
+    <div class="container-fluid">
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda consectetur distinctio error, esse impedit ipsam nemo nobis non quam, qui quos repudiandae rerum, voluptatum? Hic inventore nisi quia quos veniam!</p>
+        <p>A adipisci aliquid aut commodi consequatur debitis earum enim explicabo fuga ipsam iure molestiae necessitatibus nemo neque, nesciunt, nostrum quas quidem, quod reprehenderit similique unde veritatis voluptatem voluptatum? Accusantium, est!</p>
+        <p>A adipisci aliquid aut commodi consequatur debitis earum enim explicabo fuga ipsam iure molestiae necessitatibus nemo neque, nesciunt, nostrum quas quidem, quod reprehenderit similique unde veritatis voluptatem voluptatum? Accusantium, est!</p>
+        <p>Beatae consequatur nesciunt quam quis tempore! Distinctio eius, fuga hic laudantium minima minus officia placeat! Alias amet at consequuntur dolor eius expedita ipsa libero molestiae, nihil, obcaecati praesentium recusandae, totam?</p>
+        <p>Aspernatur dolore laborum quibusdam quidem sequi. Deleniti dignissimos eaque exercitationem fugit natus nostrum vel! Adipisci alias aliquid autem commodi dignissimos doloremque, error impedit laudantium odio officia quaerat qui reprehenderit tenetur?</p>
+        <p>Aspernatur dolore laborum quibusdam quidem sequi. Deleniti dignissimos eaque exercitationem fugit natus nostrum vel! Adipisci alias aliquid autem commodi dignissimos doloremque, error impedit laudantium odio officia quaerat qui reprehenderit tenetur?</p>
+        <p>Ab animi assumenda aut commodi delectus eligendi eos esse, eum ex excepturi illo incidunt minus nulla perferendis perspiciatis quae quia quidem repellat tempora ullam veniam veritatis vitae voluptatibus! Aliquid, eligendi.</p>
+        <p>Ab animi assumenda aut commodi delectus eligendi eos esse, eum ex excepturi illo incidunt minus nulla perferendis perspiciatis quae quia quidem repellat tempora ullam veniam veritatis vitae voluptatibus! Aliquid, eligendi.</p>
+
+    </div>
+</div>
+
+
+<?php include("_bottom.php"); ?>
+
