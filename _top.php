5a8d7d7a2b99611f33a08698b5109f62c798fc4e
david <david@ongoingworlds.com>
2015-11-12 22:33:32 +0000

    Lots of stuff

--- _top.php
+++ _top.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Housing hand</title>
+    <title>Blue Dwarf - a Red Dwarf spinoff PBEM game</title>
 
     <!-- Bootstrap -->
     <link href="css/styles.css" rel="stylesheet">
@@ -17,38 +17,31 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 
+    <!-- Facebook js sdk for social plugin -->
+
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.5&appId=1490291337968371";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+
+    <link href='https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:400,700' rel='stylesheet' type='text/css'>
+
 </head>
 <body>
 
-    <nav class="navbar navbar-inverse navbar-fixed-top">
+    <nav class="navbar">
         <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Bootstrap theme</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                        <ul class="dropdown-menu dropdown-menu-right">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li class="dropdown-header">Nav header</li>
-                            <li><a href="#">Separated link</a></li>
-                            <li><a href="#">One more separated link</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div><!--/.nav-collapse -->
+            <img class="logo" src="images/logo.png" alt="Blue Dwarf">
         </div>
+
+        <div class="socialBtns">
+            <div class="fb-like socialBtn" data-href="http://www.facebook.com/bluedwarf" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+        </div>
+
+        <div class="navbar__tall"></div>
+
     </nav>
--- index.php
+++ index.php
@@ -1,19 +1,36 @@
 <?php include("_top.php"); ?>
+<div class="bg"></div>
 
-<div class="container-wrapper">
+
+<div class="hero">
+    <img src="images/bd-ship.png" alt="The Blue Dwarf spaceship" class="bd-ship">
+
+
+</div>
+
+<div class="container-wrapper docked-bottom">
     <div class="container-fluid">
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda consectetur distinctio error, esse impedit ipsam nemo nobis non quam, qui quos repudiandae rerum, voluptatum? Hic inventore nisi quia quos veniam!</p>
-        <p>A adipisci aliquid aut commodi consequatur debitis earum enim explicabo fuga ipsam iure molestiae necessitatibus nemo neque, nesciunt, nostrum quas quidem, quod reprehenderit similique unde veritatis voluptatem voluptatum? Accusantium, est!</p>
-        <p>A adipisci aliquid aut commodi consequatur debitis earum enim explicabo fuga ipsam iure molestiae necessitatibus nemo neque, nesciunt, nostrum quas quidem, quod reprehenderit similique unde veritatis voluptatem voluptatum? Accusantium, est!</p>
-        <p>Beatae consequatur nesciunt quam quis tempore! Distinctio eius, fuga hic laudantium minima minus officia placeat! Alias amet at consequuntur dolor eius expedita ipsa libero molestiae, nihil, obcaecati praesentium recusandae, totam?</p>
-        <p>Aspernatur dolore laborum quibusdam quidem sequi. Deleniti dignissimos eaque exercitationem fugit natus nostrum vel! Adipisci alias aliquid autem commodi dignissimos doloremque, error impedit laudantium odio officia quaerat qui reprehenderit tenetur?</p>
-        <p>Aspernatur dolore laborum quibusdam quidem sequi. Deleniti dignissimos eaque exercitationem fugit natus nostrum vel! Adipisci alias aliquid autem commodi dignissimos doloremque, error impedit laudantium odio officia quaerat qui reprehenderit tenetur?</p>
-        <p>Ab animi assumenda aut commodi delectus eligendi eos esse, eum ex excepturi illo incidunt minus nulla perferendis perspiciatis quae quia quidem repellat tempora ullam veniam veritatis vitae voluptatibus! Aliquid, eligendi.</p>
-        <p>Ab animi assumenda aut commodi delectus eligendi eos esse, eum ex excepturi illo incidunt minus nulla perferendis perspiciatis quae quia quidem repellat tempora ullam veniam veritatis vitae voluptatibus! Aliquid, eligendi.</p>
+        <div class="row">
+            <div class="col-sm-6">
+                <p>Blue Dwarf is a text-based roleplaying game about the Red Dwarf’s sister ship. We continue the story by simply writing it (which we've been doing for over 15 years!).</p>
+
+            </div>
+            <div class="col-sm-6">
+                <p>We welcome new members so if you’re a fan of Red Dwarf &amp; enjoy writing please join us!</p>
+            </div>
+        </div>
+
+        <div class="text-center">
+            <div class="joinBtnWrapper">
+                <a href="http://www.ongoingworlds.com/games/270" class="btn joinBtn">Read posts, &amp; join</a>
+            </div>
+        </div>
 
     </div>
 </div>
+
+
 
 
 <?php include("_bottom.php"); ?>
