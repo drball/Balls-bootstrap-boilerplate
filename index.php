569d518299ebb4267a879a7cb839507be22bce86
david <david@ongoingworlds.com>
2015-11-13 17:16:52 +0000

    Styles for captions

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 
 <div class="hero">
-    <img src="images/bd-ship.png" alt="The Blue Dwarf spaceship" class="bd-ship animated animated-delay fadeInLeft">
+    <img src="images/bd-ship.png" alt="The Blue Dwarf spaceship" class="bd-ship">
 
 
 </div>
@@ -42,12 +42,14 @@
 
             <div class="row">
 
-                <div class="col-sm-6">
-                    <h3>Drive Room</h3>
+                <div class="col-sm-6 caption">
+
                     <img
-                        class="pull-left" title="D-droom"
+                        class="thumbnail" title="D-droom"
                         src="http://bluedwarf.co.uk/wp-content/uploads/2012/03/D-droom-150x150.jpg" alt="Drive Room"
                         width="150" height="150">
+
+                    <h3>Drive Room</h3>
 
                     <p>The nerve centre of the entire ship, and the closest thing the Blue Dwarf has to a Star Trek-style
                         “Bridge”, it is where the Command Officers make all the decisions that effect ship operations and
@@ -55,59 +57,86 @@
                         communications, navigation, engineering or scanning computers.</p>
                 </div>
 
-                <div class="col-sm-6">
-                    <h3><a href="http://bluedwarf.co.uk/?attachment_id=171" rel="attachment wp-att-171"><img
-                                class="alignleft size-thumbnail wp-image-171" title="MahonPointShoppingCentre"
-                                src="http://bluedwarf.co.uk/wp-content/uploads/2012/03/MahonPointShoppingCentre-150x150.jpg"
-                                alt="Promenade" width="150" height="150"></a>The Promenade</h3>
+                <div class="col-sm-6 caption">
+
+
+                    <img
+                        class="thumbnail" title="MahonPointShoppingCentre"
+                        src="http://bluedwarf.co.uk/wp-content/uploads/2012/03/MahonPointShoppingCentre-150x150.jpg"
+                        alt="Promenade" width="150" height="150">
+
+                    <h3>The Promenade</h3>
 
                     <p>The ship’s shopping centre is one long area lined with shops that runs almost the full length of the
                         ship. After the 3 million years in stasis all these shops are now abandoned, and the Promenade has
                         been inhabited by a collection of evolved rodents called “The Vermin”.</p>
                 </div>
+            </div>
+            <div class="row">
 
-                <div class="col-sm-6">
-                    <h3><img class="alignleft size-thumbnail wp-image-152" title="parrotts"
-                             src="http://bluedwarf.co.uk/wp-content/uploads/2012/03/parrotts-150x150.jpg" alt="Parrotts bar"
-                             width="150" height="150">Parrotts bar</h3>
+                <div class="col-sm-6 caption">
+
+
+                    <img class="thumbnail" title="parrotts"
+                         src="http://bluedwarf.co.uk/wp-content/uploads/2012/03/parrotts-150x150.jpg" alt="Parrotts bar"
+                         width="150" height="150">
+
+                    <h3>Parrotts bar</h3>
 
                     <p>Parrotts is a bar where most crewmembers go to unwind after a busy work day, or where they go to
-                        avoid a busy work day. It’s tatty, it’s a dive, but it’s the best bar on the ship.</p></div>
-                <div class="col-sm-6">
-                    <h3><img class="alignleft size-thumbnail wp-image-116" title="shuttlebay"
-                             src="http://bluedwarf.co.uk/wp-content/uploads/2012/03/shuttlebay1-150x150.jpg"
-                             alt="Blue Midgets" width="150" height="150">Flight Deck</h3>
+                        avoid a busy work day. It’s tatty, it’s a dive, but it’s the best bar on the ship.</p>
+                </div>
+
+                <div class="col-sm-6 caption">
+
+                    <img class="thumbnail" title="shuttlebay"
+                         src="http://bluedwarf.co.uk/wp-content/uploads/2012/03/shuttlebay1-150x150.jpg"
+                         alt="Blue Midgets" width="150" height="150">
+
+                    <h3>Flight Deck</h3>
 
                     <p>This is where&nbsp;all Blue Dwarf’s secondary vehicles like Starbugs and Blue Midgets are parked, and
                         where they are prepared for into going into space. An airlock opens onto a runway tunnel for
                         entering and leaving the ship.</p>
                 </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 caption">
 
-                <div class="col-sm-6">
-                    <h3><a href="http://bluedwarf.co.uk/?attachment_id=175" rel="attachment wp-att-175"><img
-                                class="alignleft size-thumbnail wp-image-175" title="medibay"
-                                src="http://bluedwarf.co.uk/wp-content/uploads/2012/03/medibay-150x150.jpg"
-                                alt="Medical beds" width="150" height="150"> </a>Medi-bay</h3>
+
+                    <img
+                        class="thumbnail" title="medibay"
+                        src="http://bluedwarf.co.uk/wp-content/uploads/2012/03/medibay-150x150.jpg"
+                        alt="Medical beds" width="150" height="150">
+
+                    <h3>Medi-bay</h3>
 
                     <p>A series of emergency rooms for looking after patients. The Medi-bay is actually detachable and
                         escaped at some point when most of the crew were in stasis, and it’s a mystery where it’s gone.
-                        Chief surgeon Dr Keto has also disappeared.<br>
+                        Chief surgeon Dr Keto has also disappeared.
                     </p>
                 </div>
-                <div class="col-sm-6">
-                    <h3><a href="http://bluedwarf.co.uk/?attachment_id=174" rel="attachment wp-att-174"><img
-                                class="alignleft size-thumbnail wp-image-174" title="main reactor"
-                                src="http://bluedwarf.co.uk/wp-content/uploads/2012/03/main-reactor-150x150.jpg"
-                                alt="Main reactor" width="150" height="150"></a>Engine decks &amp; Main Reactor</h3>
+
+
+                <div class="col-sm-6 caption">
+
+
+                    <img
+                        class="thumbnail" title="main reactor"
+                        src="http://bluedwarf.co.uk/wp-content/uploads/2012/03/main-reactor-150x150.jpg"
+                        alt="Main reactor" width="150" height="150">
+
+                    <h3>Engine decks &amp; Main Reactor</h3>
 
                     <p>Large machinery takes up much of the rear decks for the engines and vital ship functions. Many of
                         these have been flooded during the time in stasis and have become rusty and unusable. The main
                         reactor is a nuclear core held in place by vital Drive Plates, it was due to these cracking that
-                        caused the leak of Cadmium II.<br>
+                        caused the leak of Cadmium II.
                     </p>
                 </div>
-
-                <div class="col-sm-6">
+            </div>
+            <div class="row">
+                <div class="col-sm-6 caption">
                     <h3>Cargo Decks</h3>
 
                     <p>There are many huge empty cargo bays on the Blue Dwarf’s lower decks which are waiting to be filled.
@@ -115,11 +144,15 @@
                         be transporting to a destination, or just keeping for spares.<br>
                     </p>
                 </div>
-                <div class="col-sm-6">
-                    <h3><a href="http://bluedwarf.co.uk/?attachment_id=161" rel="attachment wp-att-161"><img
-                                class="alignleft size-thumbnail wp-image-161" title="EdenPROJECT"
-                                src="http://bluedwarf.co.uk/wp-content/uploads/2012/03/EdenPROJECT-150x150.jpg"
-                                alt="Arboretum" width="150" height="150"></a>Arboretum</h3>
+                <div class="col-sm-6 caption">
+
+
+                    <img
+                        class="thumbnail" title="EdenPROJECT"
+                        src="http://bluedwarf.co.uk/wp-content/uploads/2012/03/EdenPROJECT-150x150.jpg"
+                        alt="Arboretum" width="150" height="150">
+
+                    <h3>Arboretum</h3>
 
                     <p>Originally a botanical garden on the ship, this has now become overgrown in the 3 million years the
                         crew were in stasis and is now the jungle home of some evolved lizards called the Huzards.<br>
@@ -130,6 +163,16 @@
 
         </div>
 
+    </div>
+</div>
+
+
+<div class="parallax">
+    <div class="parallax__layer parallax__layer--back">
+        asdasdas s dasd asda
+    </div>
+    <div class="parallax__layer parallax__layer--base">
+        asd asd sad ads sd
     </div>
 </div>
 
